19f8c7549560abf6d4b343183cdd5b975c36d847
Tino Espinoza <espch03@ca.com>
2018-10-12 16:10:51 -0600

    catch just incase

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,15 +1,21 @@
 self.addEventListener('install', function(e) {
   e.waitUntil(
-    caches.open('mysite').then(function(cache) {
-      return cache.addAll(['./', './index.html']);
-    }),
+    caches
+      .open('mysite')
+      .then(function(cache) {
+        return cache.addAll(['./', './index.html']);
+      })
+      .catch(),
   );
 });
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    }),
+    caches
+      .match(event.request)
+      .then(function(response) {
+        return response || fetch(event.request);
+      })
+      .catch(),
   );
 });
