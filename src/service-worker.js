dc4edd14976408ef89e297d659374f2317bca029
Tino Espinoza <espch03@ca.com>
2018-10-18 14:51:34 -0600

    remove empty catches

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,21 +1,15 @@
 self.addEventListener('install', function(e) {
   e.waitUntil(
-    caches
-      .open('mysite')
-      .then(function(cache) {
-        return cache.addAll(['./index.js', './index.html', './images']);
-      })
-      .catch(),
+    caches.open('mysite').then(function(cache) {
+      return cache.addAll(['./index.js', './index.html', './images']);
+    }),
   );
 });
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches
-      .match(event.request)
-      .then(function(response) {
-        return response || fetch(event.request);
-      })
-      .catch(),
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    }),
   );
 });
