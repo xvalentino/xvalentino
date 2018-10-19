009d25d1c0ae8576323f7b567de172bdeb49deb9
Tino Espinoza <espch03@ca.com>
2018-10-19 11:12:21 -0600

    smarter caching + http localdev

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./service-worker.js').catch();
+  navigator.serviceWorker.register('./service-worker.js');
 }
--- src/service-worker.js
+++ src/service-worker.js
@@ -1,15 +1,35 @@
-// self.addEventListener('install', function(e) {
-//   e.waitUntil(
-//     caches.open('mysite').then(function(cache) {
-//       return cache.addAll(['./index.js', './index.html', './images']);
-//     }),
-//   );
-// });
+self.addEventListener('install', function(e) {});
 
-// self.addEventListener('fetch', function(event) {
-//   event.respondWith(
-//     caches.match(event.request).then(function(response) {
-//       return response || fetch(event.request);
-//     }),
-//   );
-// });
+const getRequestHash = request => {
+  console.log('request', request);
+  const splitUrl = request.url.split('.');
+  splitUrl.splice(-2, 1);
+  return splitUrl.join('.');
+};
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open('mysite-dynamic').then(function(cache) {
+      return cache.match(event.request).then(function(response) {
+        return (
+          response ||
+          fetch(event.request).then(function(response) {
+            cache.put(event.request, response.clone());
+            cache.keys().then(function(cacheNames) {
+              return Promise.all(
+                cacheNames
+                  .filter(function(cacheName) {
+                    getRequestHash(event.request) === getRequestHash(cacheName);
+                  })
+                  .map(function(cacheName) {
+                    return caches.delete(cacheName);
+                  }),
+              );
+            });
+            return response;
+          })
+        );
+      });
+    }),
+  );
+});
