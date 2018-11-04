24ad293f867d8b99653815aa13a3bf282fae9a8b
Tino Espinoza <espch03@ca.com>
2018-11-04 16:10:32 -0700

    update bio again hm

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -3,6 +3,6 @@ import Text from 'mineral-ui/Text';
 
 export default () => (
   <Text style={{ margin: 20 }} align="center">
-    Inclusion advocate. Artisan of Code and Art for fun and money.
+    Artisan of Code and Art for fun and money.
   </Text>
 );
